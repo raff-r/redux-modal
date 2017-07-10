e61647ab60ddbefd85a4da514c3d3a66d745315b
Raffaele Rizzetti <Raffaele.Rizzetti@moneysavingexpert.com>
2017-07-10 10:20:42 +0100

    add styles to package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Modal from './components/modal';
 import * as modalActions from './actions';
 import modalReducer from './reducers/modal';
+import ModalStyles from './styles/modals.css';
 
 export const modal = Modal;
 export const actions = modalActions;
 export const reducer = modalReducer;
+export const styles = ModalStyles;
